0aa777648e2de0e92e3431e642009bb113ee1fe0
Owonubi Job Sunday <jobowonubi@gmail.com>
2022-03-06 00:45:57 +0100

    Local Blockchain

--- deploy.py
+++ deploy.py
@@ -38,7 +38,6 @@ chain_id = 1337
 # My Key and Address changes always, hence I did not save them in environs. I'm using Ganache
 my_address = "0x5D5c5451193F8f1a30B9002FA10FeC4Cc08c7a10"
 private_key = os.getenv('PRIVATE_KEY')
-print(private_key)
 
 
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -52,12 +51,17 @@ nonce = w3.eth.getTransactionCount(my_address)
 # print(SimpleStorage)
 # print(SimpleStorage.constructor())
 # print(SimpleStorage.constructor().buildTransaction())
-print(w3.eth.gas_price)
 transaction = SimpleStorage.constructor().buildTransaction({
     "gasPrice": w3.eth.gas_price,
     "chainId": chain_id,
     "from": my_address,
     "nonce": nonce,
 })
-# signed_transaxtion = w3.eth.account.sign_transaction(
-#     transaction=transaction, private_key=private_key)
+signed_transaxtion = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key)
+print(signed_transaxtion)
+
+
+# Send this signed transaction
+
+tx_hash = w3.eth.send_raw_transaction(signed_transaxtion.rawTransaction)
